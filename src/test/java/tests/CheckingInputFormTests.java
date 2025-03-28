01ac3fe287a342e157026d20d401ad9160e3cf7b
Maksim Ionkin <maks.290899@inbox.ru>
2025-03-28 14:14:55 +0500

    Исправленны анотации а так же сделан allure отчет

--- src/test/java/tests/CheckingInputFormTests.java
+++ src/test/java/tests/CheckingInputFormTests.java
@@ -2,6 +2,7 @@ package tests;
 
 import com.example.Config;
 import io.qameta.allure.Step;
+import io.qameta.allure.junit4.DisplayName;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,7 +16,7 @@ public class CheckingInputFormTests {
     private WebDriverWait wait;
 
     @Before
-    @Step("Настройка тестового окружения")
+    @DisplayName("Настройка тестового окружения")
     public void setUp() {
         driver = new ChromeDriver();
         driver.get(Config.BASE_URL);
@@ -23,35 +24,35 @@ public class CheckingInputFormTests {
     }
 
     @Test
-    @Step("Тест ввода в главном меню")
+    @DisplayName("Тест ввода в главном меню")
     public void inputFormMainMenuTest() {
         InputPage inputPage = new InputPage(driver, wait);
         inputPage.inputFormMainMenu("maks123@mail.ru", "Maks4321");
     }
 
     @Test
-    @Step("Тест ввода в личном кабинете")
+    @DisplayName("Тест ввода в личном кабинете")
     public void inputFormPersonalAccountTest() {
         InputPage inputPage = new InputPage(driver, wait);
         inputPage.inputFormPersonalAccount("maks123@mail.ru", "Maks4321");
     }
 
     @Test
-    @Step("Тест ввода в форме регистрации")
+    @DisplayName("Тест ввода в форме регистрации")
     public void inputFormRegisterFormTest() {
         InputPage inputPage = new InputPage(driver, wait);
         inputPage.inputFormRegisterForm("maks123@mail.ru", "Maks4321");
     }
 
     @Test
-    @Step("Тест ввода в форме восстановления пароля")
+    @DisplayName("Тест ввода в форме восстановления пароля")
     public void inputFormRestorePasswordTest() {
         InputPage inputPage = new InputPage(driver, wait);
         inputPage.inputFormRestorePassword("maks123@mail.ru", "Maks4321");
     }
 
     @After
-    @Step("Закрытие тестового окружения")
+    @DisplayName("Закрытие тестового окружения")
     public void tearDown() {
         if (driver != null) {
             driver.quit();
